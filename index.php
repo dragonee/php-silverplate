16794c113e8d439f19ec5a9c6a574f2c306c9d38
Michał Moroz <michalmoroz@gmail.com>
2012-08-15 07:02:50 +0900

    see README.md in app.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 
 <p>This message is located in the index.php file in the main directory of your website. This file responds to your main site index, you can modify it to your needs.</p>
 
+<p>PHP Boilerplate can also render Markdown files. You can access the documentation in form of the <a href="README">README.md file</a> to see how it works.</p>
+
 <p>The layout.php file is responsible for the headers and footers of your website. In this file you can put your style declarations, JavaScripts and other data.</p>
 
 <?php open('footer') ?>
